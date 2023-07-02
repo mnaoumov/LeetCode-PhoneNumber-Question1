13b7b0fe7aa8c2eb28e3ee170ed6ee9af914a418
Michael Naumov <mnaoumov@gmail.com>
2023-07-02 15:13:12 -0600

    Reformat

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import {
-  validateInput,
-  extractPhoneNumber,
-  formatPhoneNumber
+    validateInput,
+    extractPhoneNumber,
+    formatPhoneNumber
 } from "./phoneTextBox.js";
 
 const phoneTextBox = document.getElementById("phone");
@@ -9,12 +9,12 @@ const phoneTextBox = document.getElementById("phone");
 let phoneNumber = "";
 
 phoneTextBox.addEventListener("beforeinput", e => {
-  if (!validateInput(e.data)) {
-    e.preventDefault();
-  }
+    if (!validateInput(e.data)) {
+        e.preventDefault();
+    }
 });
 
 phoneTextBox.addEventListener("input", () => {
-  phoneNumber = extractPhoneNumber(phoneTextBox.value);
-  phoneTextBox.value = formatPhoneNumber(phoneNumber);
+    phoneNumber = extractPhoneNumber(phoneTextBox.value);
+    phoneTextBox.value = formatPhoneNumber(phoneNumber);
 });
