46d806e0ae3d1da83e6125e77664c1d6420a9efe
Michael Naumov <mnaoumov@gmail.com>
2023-07-03 14:29:41 -0600

    Add React

---
+++ src/PhoneTextBox.js
@@ -0,0 +1,5 @@
+export default function PhoneTextBox() {
+    return (
+        <div>HELLO</div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import vanillaPhoneTextBox from "./vanillaPhoneTextBox.js";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import PhoneTextBox from "./PhoneTextBox.js";
 
 vanillaPhoneTextBox(document.getElementById("phone"));
+
+const rootElement = document.getElementById("reactPhoneTextBox");
+const root = createRoot(rootElement);
+
+root.render(
+    <StrictMode>
+      <PhoneTextBox />
+    </StrictMode>
+  );
