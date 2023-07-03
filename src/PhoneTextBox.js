119ed6d0a497afb6a2393dcd01799cf747c39203
Michael Naumov <mnaoumov@gmail.com>
2023-07-03 14:35:08 -0600

    Add markup

--- src/PhoneTextBox.js
+++ src/PhoneTextBox.js
@@ -1,5 +1,12 @@
+import { useId } from "react";
+
 export default function PhoneTextBox() {
+    const id = useId();
+
     return (
-        <div>HELLO</div>
+        <div className="container text-center">
+            <input type="tel" id={id} maxLength="16" placeholder="mobile number" autoComplete="off" />
+            <div><label htmlFor={id}>(123) 456-7890</label></div>
+        </div>
     );
 }
