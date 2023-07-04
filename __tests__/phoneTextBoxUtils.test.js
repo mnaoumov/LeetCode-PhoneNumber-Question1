a6e0dca0d7edccb4c10c3fa5cdaef8a00155c97d
Michael Naumov <mnaoumov@gmail.com>
2023-07-03 20:03:20 -0600

    Use lowercase

--- __tests__/phoneTextBoxUtils.test.js
+++ __tests__/phoneTextBoxUtils.test.js
@@ -5,71 +5,71 @@ import {
 } from "../src/phoneTextBoxUtils.js";
 
 describe("validateInput", () => {
-    it("Returns true for empty input", () => {
+    it("returns true for empty input", () => {
         expect(validateInput("")).toBe(true);
     });
 
-    it("Returns true for null input", () => {
+    it("returns true for null input", () => {
         expect(validateInput(null)).toBe(true);
     });
 
-    it("Returns true for single digits", () => {
+    it("returns true for single digits", () => {
         for (const inputData of ["0", "5", "9"]) {
             expect(validateInput(inputData)).toBe(true);
         }
     });
 
-    it("Returns false for non-digits", () => {
+    it("returns false for non-digits", () => {
         for (const inputData of ["A", "a", "Z", "z", ".", "/"]) {
             expect(validateInput(inputData)).toBe(false);
         }
     });
 
-    it("Returns true for multi-digit strings", () => {
+    it("returns true for multi-digit strings", () => {
         for (const inputData of ["12", "123", "1234567890"]) {
             expect(validateInput(inputData)).toBe(true);
         }
     });
 
-    it("Returns true for strings that contain non-digits", () => {
+    it("returns true for strings that contain non-digits", () => {
         for (const inputData of ["12a", "12.3", "12345b67890"]) {
             expect(validateInput(inputData)).toBe(false);
         }
     });
 
-    it("Returns true for long multi-digit strings", () => {
+    it("returns true for long multi-digit strings", () => {
         expect(validateInput("123456789012345678901234567890")).toBe(true);
     });
 });
 
 describe("extractPhoneNumber", () => {
-    it("Removes non-digits", () => {
+    it("removes non-digits", () => {
         expect(extractPhoneNumber("1A2a3Z4a5.6/7-8,9@0#$")).toBe("1234567890");
     });
 
-    it("Trims too long strings", () => {
+    it("trims too long strings", () => {
         expect(extractPhoneNumber("12345678901234567890")).toBe("1234567890");
     });
 });
 
 describe("formatPhoneNumber", () => {
-    it("Returns empty for empty", () => {
+    it("returns empty for empty", () => {
         expect(formatPhoneNumber("")).toBe("");
     });
 
-    it("Returns code for short numbers", () => {
+    it("returns code for short numbers", () => {
         expect(formatPhoneNumber("1")).toBe("1");
         expect(formatPhoneNumber("12")).toBe("12");
         expect(formatPhoneNumber("123")).toBe("123");
     });
 
-    it("Wraps code with parentheses", () => {
+    it("wraps code with parentheses", () => {
         expect(formatPhoneNumber("1234")).toBe("(123) 4");
         expect(formatPhoneNumber("12345")).toBe("(123) 45");
         expect(formatPhoneNumber("123456")).toBe("(123) 456");
     });
 
-    it("Adds hyphen between parts", () => {
+    it("adds hyphen between parts", () => {
         expect(formatPhoneNumber("1234567")).toBe("(123) 456-7");
         expect(formatPhoneNumber("12345678")).toBe("(123) 456-78");
         expect(formatPhoneNumber("123456789")).toBe("(123) 456-789");
