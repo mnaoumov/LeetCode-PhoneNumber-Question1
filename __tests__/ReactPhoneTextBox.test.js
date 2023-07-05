04be6b3b749c5afeab6a5cc84fe09844a41b4f05
Michael Naumov <mnaoumov@gmail.com>
2023-07-04 20:32:25 -0600

    Add React test

--- __tests__/ReactPhoneTextBox.test.js
+++ __tests__/ReactPhoneTextBox.test.js
@@ -10,6 +10,12 @@ import * as phoneTextBoxUtilsModule from '../src/phoneTextBoxUtils';
 
 let textBox;
 
+function triggerInput() {
+  act(() => {
+    textBox.dispatchEvent(new InputEvent('input', { bubbles: true }));
+  });
+}
+
 beforeEach(async () => {
   render(<ReactPhoneTextBox />);
   textBox = await screen.findByRole('textbox');
@@ -35,17 +41,25 @@ it('calls validateInput on beforeInput', () => {
 it('calls extractPhoneNumber on input', () => {
   jest.spyOn(phoneTextBoxUtilsModule, phoneTextBoxUtilsModule.extractPhoneNumber.name);
   textBox.value = '3c4d';
-  act(() => {
-    textBox.dispatchEvent(new InputEvent('input', { bubbles: true }));
-  });
+  triggerInput();
   expect(phoneTextBoxUtilsModule.extractPhoneNumber).toBeCalledWith('3c4d');
 });
 
 it('calls formatPhoneNumber on input', () => {
   jest.spyOn(phoneTextBoxUtilsModule, phoneTextBoxUtilsModule.formatPhoneNumber.name);
   jest.spyOn(phoneTextBoxUtilsModule, phoneTextBoxUtilsModule.extractPhoneNumber.name).mockReturnValue('5e6f');
-  act(() => {
-    textBox.dispatchEvent(new InputEvent('input', { bubbles: true }));
-  });
+  triggerInput();
   expect(phoneTextBoxUtilsModule.formatPhoneNumber).toBeCalledWith('5e6f');
 });
+
+it(
+  'sets formattedPhoneNumber from formatPhoneNumber',
+  () => {
+    jest.spyOn(phoneTextBoxUtilsModule, phoneTextBoxUtilsModule.formatPhoneNumber.name)
+      .mockReturnValue('(123) 45');
+
+    triggerInput();
+
+    expect(textBox.value).toBe('(123) 45');
+  },
+);
